86e9f6948e88c02914b28c065dd15b610c56d8da
Taylor Hughes <taylor hughes>
2021-12-01 08:54:04 -0800

    cleaned up console logging

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,6 @@ const auth = require("../auth")
 router.get('/', auth, async (req, res) => {
     const token = req.headers.authorization.split(" ")[1]
     const payload = await jwt.verify(token, process.env.TOKEN_SECRET)
-    console.log(payload, "this is working");
     const user = await User.findById(JSON.parse(payload.data)._id)
     res.json({
         status: 200,
